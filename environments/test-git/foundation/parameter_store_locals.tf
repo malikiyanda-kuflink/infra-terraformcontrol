7bb874eb31efae9e51b6cccb8a49494274fd930a
Malik <m.iyanda@kuflink.com>
2025-09-25 13:49:19 +0100

    foundation- simple flow

--- environments/test-git/foundation/parameter_store_locals.tf
+++ environments/test-git/foundation/parameter_store_locals.tf
@@ -7,6 +7,13 @@ locals {
   kuflink_office_ips = jsondecode(data.aws_ssm_parameter.office_ips.value)
   dbt_cloud_ips = jsondecode(data.aws_ssm_parameter.dbt_cloud_ips.value)
 
+  fivetran_gcp = [
+    {
+      description = "FiveTran GCP CIDR (europe-west2 (London))"
+      cidr        = data.aws_ssm_parameter.fivetran_gcp_ip
+    }
+  ]
+
 
   # kuflink_office_ips = [
   #   {                                                                               
@@ -40,12 +47,7 @@ locals {
 
   
 
-  fivetran_gcp = [
-    {
-      description = "FiveTran GCP CIDR (europe-west2 (London))"
-      cidr        = data.aws_ssm_parameter.fivetran_gcp_ip
-    }
-  ]
+
 
 }
 
