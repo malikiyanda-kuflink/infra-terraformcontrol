8b9b07fd5f6516384acd34643c009b28de4da3f3
Malik <m.iyanda@kuflink.com>
2025-09-26 16:22:08 +0100

    site to site vpn added

--- environments/test-git/foundation/variables.tf
+++ environments/test-git/foundation/variables.tf
@@ -13,3 +13,23 @@ variable "module_versions" {
   }
 }
 
+# -----------------------------
+# Variables (override in tfvars)
+# -----------------------------
+variable "onprem_public_ip" {
+  description = "Static public IP of the on-prem firewall/router"
+  default = "89.197.135.242"
+  type        = string
+}
+
+variable "onprem_cidrs" {
+  description = "List of on-premise CIDRs to route into AWS"
+  type        = list(string)
+  # If you want a default, it must be a LIST:
+  default     = ["10.0.0.0/16"]
+}
+
+variable "private_route_table_ids" {
+  type    = list(string)
+  default = []
+}
