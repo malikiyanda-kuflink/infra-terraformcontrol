1baa140839977b4c61594394c3c4545051560620
Malik <m.iyanda@kuflink.com>
2025-09-27 21:48:27 +0100

    Add parameter store configuration with environment variable

--- environments/test-git/foundation/parameter_store.tf
+++ environments/test-git/foundation/parameter_store.tf
@@ -1,4 +1,4 @@
 module "secrets" {
-  source = "git::ssh://git@github.com/malikiyanda-kuflink/infra-terraformcontrol.git//modules/parameter-store?ref=v0.1.68"
+  source = "git::ssh://git@github.com/malikiyanda-kuflink/infra-terraformcontrol.git//modules/parameter-store?ref=v0.1.69"
   environment = local.env
 }
