4ec4cec944655473eb5e3be29c8032a11b66bef7
Malik <m.iyanda@kuflink.com>
2025-09-27 12:40:35 +0100

    cleanup

--- environments/test-git/foundation/parameter_store_locals.tf
+++ environments/test-git/foundation/parameter_store_locals.tf
@@ -13,41 +13,5 @@ locals {
       cidr        = data.aws_ssm_parameter.fivetran_gcp_ip
     }
   ]
-
-
-  # kuflink_office_ips = [
-  #   {                                                                               
-  #     description = "Gravesend Backup Line"
-  #     cidr        = "82.152.39.92/32"
-  #   },
-  #   {
-  #     description = "Gravesend Docklands Office"
-  #     cidr        = "109.72.216.250/32"
-  #   },
-  #   {
-  #     description = "Gravesend Main Office"
-  #     cidr        = "89.197.135.242/32"
-  #   }
-  # ]
-
-  # dbt_cloud_ips = [
-  #   {
-  #     description = "DBT cloud IP"
-  #     cidr        = "54.81.134.249/32"
-  #   },
-  #   {
-  #     description = "DBT cloud IP"
-  #     cidr        = "52.45.144.63/32"
-  #   },
-  #   {
-  #     description = "DBT cloud IP"
-  #     cidr        = "34.233.79.135/32"
-  #   }
-  # ]
-
-  
-
-
-
 }
 
