8ddfd4a2ba26e606e4e60153429e80eec4f67011
Malik <m.iyanda@kuflink.com>
2025-09-27 23:47:41 +0100

    missing scripts added

--- environments/test-git/apps/eb-api.tf
+++ environments/test-git/apps/eb-api.tf
@@ -1,6 +1,6 @@
 # Associate WAF -> EB ALB (only when both are present)
 resource "aws_wafv2_web_acl_association" "eb_alb" {
-  count = local.enable_eb && local.enable_eb_waf && local.eb_web_acl_arn != null && length(data.aws_resourcegroupstaggingapi_resources.eb_alb.resource_tag_mapping_list) > 0 ? 1 : 0
+  count = local.enable_eb && local.enable_eb_waf && local.eb_web_acl_arn != null && local.eb_alb_arn != null ? 1 : 0
 
   resource_arn = local.eb_alb_arn
   web_acl_arn  = local.eb_web_acl_arn
