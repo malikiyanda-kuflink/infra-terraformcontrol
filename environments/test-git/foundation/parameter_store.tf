ef410641cfde2cb83ac194431ebd85406a304d69
Malik <m.iyanda@kuflink.com>
2025-09-29 21:43:44 +0100

    keys updated in ssm

--- environments/test-git/foundation/parameter_store.tf
+++ environments/test-git/foundation/parameter_store.tf
@@ -1,4 +1,4 @@
 module "secrets" {
-  source = "git::ssh://git@github.com/malikiyanda-kuflink/infra-terraformcontrol.git//modules/parameter-store?ref=v0.1.71"
+  source = "git::ssh://git@github.com/malikiyanda-kuflink/infra-terraformcontrol.git//modules/parameter-store?ref=v0.1.78"
   environment = local.env
 }
