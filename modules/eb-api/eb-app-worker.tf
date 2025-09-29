dd74fdbf50658a2c78affe86d1be26e04928f31e
Malik <m.iyanda@kuflink.com>
2025-09-29 23:10:04 +0100

    eb  update

--- modules/eb-api/eb-app-worker.tf
+++ modules/eb-api/eb-app-worker.tf
@@ -6,9 +6,9 @@ resource "aws_sqs_queue" "worker_queue" {
   visibility_timeout_seconds = var.sqsd_visibility_timeout
   tags                       = var.tags
 
-  # lifecycle {
-  #   prevent_destroy = false
-  # }
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 # =================================================================================================
@@ -20,9 +20,9 @@ resource "aws_elastic_beanstalk_environment" "worker_env" {
   solution_stack_name = var.solution_stack_name
   tier                = var.tier
 
-  lifecycle {
-    ignore_changes = [setting] # TEMPORARY; remove once stable
-  }
+  # lifecycle {
+  #   ignore_changes = [setting] # TEMPORARY; remove once stable
+  # }
 
   # -------------------------------------------
   # Service Access (roles, profiles, key, SGs, SSH)
