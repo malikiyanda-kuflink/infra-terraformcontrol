883528b5d232065928de6727962bc94d13694ab9
Malik <m.iyanda@kuflink.com>
2025-10-06 09:37:57 +0100

    waf dahsbaord name change

--- environments/test-git/apps/eb-waf.tf
+++ environments/test-git/apps/eb-waf.tf
@@ -1,6 +1,6 @@
 module "eb_waf" {
   count       = local.enable_eb_waf ? 1 : 0
-  source      = "git::ssh://git@github.com/malikiyanda-kuflink/infra-terraformcontrol.git//modules/eb-waf?ref=v0.1.96"
+  source      = "git::ssh://git@github.com/malikiyanda-kuflink/infra-terraformcontrol.git//modules/eb-waf?ref=v0.1.97"
   name_prefix = local.name_prefix
   name_prefix_upper = local.name_prefix_upper
   environment = local.environment
