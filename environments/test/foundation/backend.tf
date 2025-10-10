e19a1a38885523c69693267823528d8bcf1c25f1
Malik <m.iyanda@kuflink.com>
2025-10-10 09:03:20 +0100

    workfloww

--- environments/test/foundation/backend.tf
+++ environments/test/foundation/backend.tf
@@ -1,5 +1,9 @@
-# environments/test/foundation/backend.tf
 terraform {
   backend "s3" {
+    bucket         = "kuflink-test-states"
+    key            = "test/foundation/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "kuflink-tf-locks-test"
+    encrypt        = true
   }
 }
