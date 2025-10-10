4196e38d89b4c92e1c5d26b158fcd985f878dab4
Malik <m.iyanda@kuflink.com>
2025-10-10 17:25:20 +0100

    platform for state

--- environments/test/platform/main.tf
+++ environments/test/platform/main.tf
@@ -1,5 +1,5 @@
 data "aws_ssm_parameter" "aws_region" { name = "/backend/staging/AWS_REGION" }
 output "aws_region" {
-  value = data.aws_ssm_parameter.aws_region.value
+  value     = data.aws_ssm_parameter.aws_region.value
   sensitive = true
 }
