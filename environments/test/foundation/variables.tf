49773949b8b434d2d3a2ffe7d95962feabc9b910
Malik <m.iyanda@kuflink.com>
2025-10-18 21:39:53 +0100

    build all- fix ssm params for eb

--- environments/test/foundation/variables.tf
+++ environments/test/foundation/variables.tf
@@ -1,22 +1,5 @@
-# variables.tf (environment repos)
-variable "module_versions" {
-  type = object({
-    vpc = string
-    iam = string
-    rds = string
-    # add others
-  })
-  default = {
-    vpc = "0.2.1-rc1"
-    iam = "0.1.0"
-    rds = "0.3.2"
-  }
-}
-
 variable "environment" {
   type        = string
   description = "Deployment environment (test | staging | production)"
   default     = "test"
 }
-
-#Tigger 
