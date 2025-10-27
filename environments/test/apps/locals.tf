7ed40f6ea225e67dba1845a105d6c2fa97b7dd53
Malik <m.iyanda@kuflink.com>
2025-10-27 11:15:41 +0000

    only dbt and bastion in apps

--- environments/test/apps/locals.tf
+++ environments/test/apps/locals.tf
@@ -6,23 +6,18 @@ locals {
   # --------------------------------------
   # Comptue layer locals (controls)
   # --------------------------------------
-
-  enable_eb     = false
-  enable_eb_waf = false
-
-  enable_redis         = false
-  enable_bastion       = true
-  enable_bastion_dns   = true
-  enable_dbt           = true
-  enable_wordpress     = false
-  enable_test_instance = false
-  enable_metabase      = false
-
-  # --- Frontend/Admin S3 toggles --- # flip to true/false to skip creating the stacks
-  enable_s3_admin     = true
-  enable_s3_admin_waf = true
-
-  enable_s3_frontend         = true
+  enable_eb                  = false
+  enable_eb_waf              = false
+  enable_redis               = false
+  enable_bastion             = true
+  enable_bastion_dns         = true
+  enable_dbt                 = true
+  enable_wordpress           = false
+  enable_test_instance       = false
+  enable_metabase            = false
+  enable_s3_admin            = false # --- Frontend/Admin S3 toggles --- # flip to true/false to skip creating the stacks
+  enable_s3_admin_waf        = false
+  enable_s3_frontend         = false
   serve_frontend_maintenance = false
-
+  
 }
