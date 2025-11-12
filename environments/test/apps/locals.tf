563839fc06e188e1cf9916acaf7acf52c91c1c57
Malik <m.iyanda@kuflink.com>
2025-11-12 13:20:06 +0000

    enable redshift

--- environments/test/apps/locals.tf
+++ environments/test/apps/locals.tf
@@ -7,23 +7,23 @@ locals {
   # Comptue layer locals (controls)
   # --------------------------------------
   # --- Bastion Host toggles --- # flip to true/false to skip creating the stacks
-  enable_bastion             = true
-  enable_bastion_dns         = true
+  enable_bastion     = true
+  enable_bastion_dns = true
 
   # --- API Platform toggles --- # flip to true/false to skip creating the stacks
-  enable_eb                  = true
-  enable_eb_waf              = true
-  enable_redis               = true
+  enable_eb     = true
+  enable_eb_waf = true
+  enable_redis  = true
 
   # --- Frontend/Admin S3 toggles --- # flip to true/false to skip creating the stacks
-  enable_s3_admin            = true 
+  enable_s3_admin            = true
   enable_s3_admin_waf        = true
   enable_s3_frontend         = true
   serve_frontend_maintenance = false
 
   # --- EC2 Site toggles --- # flip to true/false to skip creating the stacks
-  enable_dbt                 = true
-  enable_metabase            = false
-  enable_wordpress           = false
-  enable_test_instance       = false
+  enable_dbt           = true
+  enable_metabase      = false
+  enable_wordpress     = false
+  enable_test_instance = false
 }
